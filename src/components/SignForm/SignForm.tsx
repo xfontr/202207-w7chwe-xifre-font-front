bd8ed9282004fc99fc61367b11677f3e8f00e3eb
Xifre Font <xifre.font.97@gmail.com>
2022-08-20 09:14:13 +0200

    Added a state to the form so that it retreives every input value

--- src/components/SignForm/SignForm.tsx
+++ src/components/SignForm/SignForm.tsx
@@ -1,20 +1,41 @@
+import { useState } from "react";
 import { InputStyled, LabelStyled, SignFormStyled } from "./SignFormStyled";
 
 interface SignFormProps {
   isSignIn: boolean;
 }
 
+const initialState = {
+  name: "",
+  password: "",
+  image: "",
+  biography: "",
+};
+
 const SignForm = ({ isSignIn }: SignFormProps): JSX.Element => {
+  const [inputs, setInputs] = useState(initialState);
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>): void => {
+    setInputs({ ...inputs, [`${event.target.name}`]: event.target.value });
+  };
+
+  const handleSubmit = (): void => {};
+
   return (
-    <SignFormStyled>
+    <SignFormStyled onSubmit={handleSubmit}>
       <div className="form-group">
         <LabelStyled htmlFor="field--name">Name</LabelStyled>
         <InputStyled
           type="text"
           id="field--name"
+          name="name"
           placeholder="Your name"
           required={true}
           autoComplete="off"
+          value={inputs.name}
+          onChange={(event) => {
+            handleChange(event);
+          }}
         />
       </div>
 
@@ -23,9 +44,14 @@ const SignForm = ({ isSignIn }: SignFormProps): JSX.Element => {
         <InputStyled
           type="password"
           id="field--password"
+          name="password"
           placeholder="Make sure it's strong ;)"
           required={true}
           autoComplete="off"
+          value={inputs.password}
+          onChange={(event) => {
+            handleChange(event);
+          }}
         />
       </div>
       {!isSignIn && (
@@ -35,9 +61,14 @@ const SignForm = ({ isSignIn }: SignFormProps): JSX.Element => {
             <InputStyled
               type="text"
               id="field--image"
+              name="image"
               placeholder="Url to your profile image"
               required={true}
               autoComplete="off"
+              value={inputs.image}
+              onChange={(event) => {
+                handleChange(event);
+              }}
             />
           </div>
 
@@ -48,9 +79,14 @@ const SignForm = ({ isSignIn }: SignFormProps): JSX.Element => {
             <InputStyled
               type="text"
               id="field--biography"
+              name="biography"
               placeholder="Hi! I like turtles."
               required={true}
               autoComplete="off"
+              value={inputs.biography}
+              onChange={(event) => {
+                handleChange(event);
+              }}
             />
           </div>
         </>

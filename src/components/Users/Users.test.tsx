ee1daf42d54b0ae712489c7befecb3a6f12247ed
Xifre Font <xifre.font.97@gmail.com>
2022-08-20 23:58:14 +0200

    Tested Users component

---
+++ src/components/Users/Users.test.tsx
@@ -0,0 +1,22 @@
+import { render } from "../../test-utils/renderWrap";
+import { screen } from "@testing-library/react";
+import Users from "./Users";
+import mockUser from "../../test-utils/mocks/mockUser";
+
+describe("Given a Users component", () => {
+  describe("When instantiated", () => {
+    test("It should show a list of users", () => {
+      render(<Users />);
+
+      const card = [];
+      card.push(
+        screen.getAllByRole("heading", { name: mockUser.name, level: 3 })
+      );
+      card.push(screen.getAllByAltText(mockUser.name));
+      card.push(screen.getAllByRole("button", { name: "Friends" }));
+      card.push(screen.getAllByRole("button", { name: "Enemies" }));
+
+      card.forEach((element) => expect(element[0]).toBeInTheDocument());
+    });
+  });
+});

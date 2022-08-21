71b1fe0796ad636582da7b4c50f59a9044c3933e
Xifre Font <xifre.font.97@gmail.com>
2022-08-21 10:00:37 +0200

    Fixed home always redirecting to itself

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,6 +1,19 @@
+import { useEffect } from "react";
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { RootState } from "../../app/store";
 import Users from "../../components/Users/Users";
 
 const HomePage = (): JSX.Element => {
+  const navigate = useNavigate();
+  const user = useSelector((state: RootState): any => state.users);
+
+  useEffect(() => {
+    if (!user) {
+      navigate("/sign-in");
+    }
+  }, [navigate, user]);
+
   return (
     <>
       <h2>User list</h2>
